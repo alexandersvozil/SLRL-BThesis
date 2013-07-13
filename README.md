ac5b5358699ea479b6fefa7fc1ef34c0a201f200
Alexander Svozil <alexander.svozil@gmail.com>
2013-07-13 17:19:47 +0200

    began parsing for the names

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-The GIT - Repo for my Bachelor Thesis on theServer Location Problem with Restricted Loads on Servers and Links (SLRL).
+The GIT - Repo for my Bachelor Thesis on the Server Location Problem with Restricted Loads on Servers and Links (SLRL).
+
 Interesting Links:
 http://www.computer.org/csdl/proceedings/saint/2011/4423/00/4423a513-abs.html
+https://www.ads.tuwien.ac.at/w/Praktika_und_Diplomarbeiten
--- src/main/java/ParseTestInstances.java
+++ src/main/java/ParseTestInstances.java
@@ -1,3 +1,6 @@
+
+import org.apache.log4j.Logger;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.List;
@@ -11,16 +14,50 @@ import java.util.Scanner;
  * To change this template use File | Settings | File Templates.
  */
 public class ParseTestInstances {
+    private final Logger log  = Logger.getLogger(ParseTestInstances.class);
 
     /**
-     * Parses inc.txt taken from quaida.com
+     * Parses inc.txt into SLRLInstance Objects taken from quaida.com
      * @return List of SLRL Test instances
      */
     public List<SLRLInstance> parse(){
+        boolean beginningSeparator = true;
         try {
             Scanner sc = new Scanner(new File("inc.txt"));
+            while(sc.hasNext()){
+                String curWord = sc.next();
+                //comment
+                if(curWord.contains("#")){
+                    sc.nextLine();
+                }
+                else{
+
+                    if(curWord.contains("::")){
+                        if(beginningSeparator == true){
+                            curWord = sc.next();
+                            String barboneName = "";
+                            while(!curWord.contains("::") && !curWord.contains("(") ){
+                                barboneName+=" " + curWord;
+                                curWord = sc.next();
+                            }
+                            System.out.println(barboneName);
+                            beginningSeparator = false;
+
+                        }
+                        else {
+                            beginningSeparator = true;
+                        }
+
+                    }
+
+                }
+
+
+
+
+            }
         } catch (FileNotFoundException e) {
-            log.error
+            log.error("File inc.txt  was not found");
         }
         return null;
     }
