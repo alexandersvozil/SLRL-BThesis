902023cd9339cfb9d519aa85297b5d1cc97d0532
Alexander Svozil <alexander.svozil@gmail.com>
2013-07-14 12:57:59 +0200

    main

---
+++ src/main/java/Application.java
@@ -0,0 +1,23 @@
+import org.apache.log4j.Logger;
+
+import java.util.List;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: svozil
+ * Date: 7/13/13
+ * Time: 4:45 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Application {
+    public static void main (String args[]) {
+        final Logger log = Logger.getLogger(Application.class);
+
+        ParseTestInstances parseTestInstances = new ParseTestInstances();
+        List<SLRLInstance> instanceList = parseTestInstances.parse();
+        log.debug("Instance Size: "+instanceList.size());
+        for(SLRLInstance slrlInstance: instanceList){
+            log.debug(slrlInstance.toString());
+        }
+    }
+}
